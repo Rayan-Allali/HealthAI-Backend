ebe2d1d6fd4845118ee04691772a5db0ddf85baa
Rayan-Allali <108684980+Rayan-Allali@users.noreply.github.com>
2022-12-25 21:03:07 +0100

    auth2'

--- routes/nurseRoutes.js
+++ routes/nurseRoutes.js
@@ -2,7 +2,7 @@
 const express=require('express');
 const router=express.Router();
 const nurseControllers = require('./../controllers/nurseController');
-
+const authControllers=require('./../controllers/authController');
 router.route('/').get(nurseControllers.getAllNurses).post(nurseControllers.AddNurse)
 router.route('/:id').get(nurseControllers.getNurse).delete(nurseControllers.deleteNurse)
 module.exports = router;
